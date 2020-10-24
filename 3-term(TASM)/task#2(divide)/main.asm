098371f4e49c17e7e88eb014aa6bf032b36548a9
4llower <95350098@study.bsuir.by>
2020-10-25 02:03:14 +0300

    updated

--- 3-term(TASM)/task#2(divide)/main.asm
+++ 3-term(TASM)/task#2(divide)/main.asm
@@ -16,7 +16,7 @@ remove_symbol proc C near
     ret
 remove_symbol endp
 
-get_number proc C near
+read_number proc C near
 uses bx, cx, dx
     mov bx, 0
     mov cx, 0
@@ -32,6 +32,9 @@ uses bx, cx, dx
 
         cmp al, 27; esc key clicked check
         je esc_clicked
+
+        cmp al, 13; enter key clicked check
+        je return_read_number
         
         ; compare on correct symbol entering
         cmp al, '0'
@@ -41,40 +44,52 @@ uses bx, cx, dx
 
         ; ** Magic **
 
-        mov last_symbol, al; save last symbol because working with ax(al changes)
-        
-        mov ax, bx; overflow check
+        cmp al, '0' ; case then we have a prefix of '0'
+        ja logic
 
-        mul ten
-        jc read_cycle ; 1st check
+        cmp cx, 0
+        je logic
 
-        mov dx, 0
-        mov dl, last_symbol
-        sub dl, '0'
-        cmp dl, 2
-        add ax, dx
-        jc read_cycle ; 2nd check
+        cmp bx, 0
+        jnz logic
 
-        mov bx, ax ; if we have no overflow add digit to number
-        
-        add cx, 1 ; add 1 to length of number
-        jmp symbol_write ; writing symbol
+        jmp read_cycle
+
+        logic:
+            mov last_symbol, al; save last symbol because working with ax(al changes)
+            
+            mov ax, bx; overflow check
+
+            mov dx, 10
+            mul dx
+            jc read_cycle ; 1st check
+
+            mov dx, 0
+            mov dl, last_symbol
+            sub dl, '0'
+
+            add ax, dx
+
+            jc read_cycle ; 2nd check
+
+            mov bx, ax ; if we have no overflow add digit to number
+            
+            inc cx; add 1 to length of number
+            jmp symbol_write ; writing symbol
 
         backspace_clicked:
             cmp cx, 0 ; check on empty string
             je read_cycle
 
-             ; remove last symbol 
+            mov dx, 0
+            mov ax, bx  
+            mov bx, 10
+            div bx
+            mov bx, ax
 
+            call remove_symbol ; remove last symbol
 
-            call remove_symbol
-              mov ax, bx
-              div ten
-              mov bx, ax
-            ;   div ten
-            ;   mov bx, ah
-
-            add cx, -1 ; decrease length of number
+            dec cx ; decrease length of number
 
         jmp read_cycle ; start new iteration
 
@@ -95,14 +110,60 @@ uses bx, cx, dx
 
         jmp read_cycle
 
-    ret
-get_number endp
+    return_read_number:
+        
+        mov dl, 0Ah;'\n' start
+        mov ah, 02h
+        int 21h
 
+        mov dl, 0Dh
+        mov ah, 02h
+        int 21h; '\n' end
+
+        mov ax, bx; move result to ax
+        ret
+read_number endp
+
+print_number proc C near 
+arg number: word
+uses ax, bx, cx, dx
+    mov ax, number
+    mov cx, 0
+
+    fill_stack:
+        mov dx, 0 ; divide on ten
+        mov bx, 10
+        div bx 
+        
+        push dx; save digit in stack
+        inc cx        
+
+        cmp ax, 0 ; check if ax equal zero
+        je write_cycle
+    jmp fill_stack
+
+    write_cycle:
+        cmp cx, 0
+        je return_print_number
+
+        pop dx
+        add dx, '0'
+        mov ah, 02h
+        int 21h
+
+        dec cx
+    jmp write_cycle
+
+    return_print_number:
+        ret
+print_number endp
 main:
     mov ax, @data ; initialize data segment
     mov ds, ax 
 
-    call get_number
+    call read_number
+
+    call print_number C, ax
 
     return: 
         mov ah, 4ch ; exit interrupt

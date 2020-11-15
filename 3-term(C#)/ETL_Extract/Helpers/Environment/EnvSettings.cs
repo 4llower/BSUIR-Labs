9d3946a97c1c19b8e98e9cff80bbbba4f8641deb
4llower <i.4llower@gmail.com>
2020-11-15 20:54:54 +0300

    add: init 2nd lab (C#)

---
+++ 3-term(C#)/ETL_Extract/Helpers/Environment/EnvSettings.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ETL_Extract.Helpers
+{
+    static public class EnvSettings
+    {
+        public static dynamic Env { get; set; }
+        private static readonly string pathToSettings = "../../../appsettings.json";
+        // use at launch application
+        static public void Initialize()
+        {
+            StreamReader stream = new StreamReader(pathToSettings);
+            Env = JsonConvert.DeserializeObject(stream.ReadToEnd());
+        }
+    }
+}
---
+++ 3-term(C#)/ETL_Extract/Index.cs
@@ -0,0 +1,13 @@
+﻿using ETL_Extract.Helpers;
+using System;
+
+namespace ETL_Extract
+{
+    class Index
+    {
+        static void Main(string[] args)
+        {
+            EnvSettings.Initialize();
+        }
+    }
+}

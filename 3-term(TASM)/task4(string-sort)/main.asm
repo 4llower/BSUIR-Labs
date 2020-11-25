f0a2290924f487d3c1f9a1f20d9a5f653944b710
4llower <95350098@study.bsuir.by>
2020-11-25 20:22:58 +0300

    add: 4th lab

--- 3-term(TASM)/task4(string-sort)/main.asm
+++ 3-term(TASM)/task4(string-sort)/main.asm
@@ -1,27 +1,53 @@
 .model small
 .stack 100h
 .data
-    
+    count dw 26 dup(0)
 .code
-
-write_carryover proc C near
-uses ax, dx
-    mov dl, 0Ah;'\n' start
-    mov ah, 02h
-    int 21h
-
-    mov dl, 0Dh
-    mov ah, 02h
-    int 21h; '\n' end
-    ret
-write_carryover endp
-
 main:
     mov ax, @data ; initialize data segment
     mov ds, ax
 
-    
+    read_cycle:
+        mov ah, 01h
+        int 21h
+        
+        cmp al, 10
+        je break_read_cycle
+        cmp al, 13
+        je break_read_cycle   
+        
+        cbw
+        mov si, ax
+        sub si, 'a'
+        inc [count[si]]
+    jmp read_cycle
 
+    break_read_cycle:
+        mov cx, 26
+        count_cycle:
+            cmp cx, 0
+            je return
+
+            mov ax, 26
+            sub ax, cx  
+
+            mov si, ax
+            mov bl, byte ptr count[si]
+
+            output_cycle:
+                cmp bl, 0
+                je break
+                mov dx, si
+                add dx, 'a'
+                mov ah, 02h
+                int 21h
+                
+                dec bl
+            jmp output_cycle
+
+            break:
+                dec cx
+        jmp count_cycle
     return: 
         mov ah, 4ch ; exit interrupt
         int 21h

3f5f47c1b94dc73bb937bef9e9b54061ef229526
4llower <95350098@study.bsuir.by>
2021-03-08 20:46:09 +0300

    add: tests

---
+++ 4-term(Numeric-Analysis)/first/main.py
@@ -0,0 +1,19 @@
+def gauss_default_solve():
+    pass
+
+
+def gauss_partial_selection_solve():
+    pass
+
+
+def gauss_full_selection_solve():
+    pass
+
+
+def main():
+    print('hello world!')
+    pass
+
+
+if __name__ == '__main__':
+    main()
